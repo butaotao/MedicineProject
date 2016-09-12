f9450b5e8e8934ce23fe6e4fecc9f3e9f833e934
butaotao <454714716@qq.com>
2016-09-12 15:12:51 +0800

    Signed-off-by: butaotao <454714716@qq.com>

--- dgroupDoctorCompany/src/main/java/com/dachen/dgroupdoctorcompany/activity/ManagerColleagueActivity.java
+++ dgroupDoctorCompany/src/main/java/com/dachen/dgroupdoctorcompany/activity/ManagerColleagueActivity.java
@@ -90,31 +90,25 @@ public class ManagerColleagueActivity extends CompanyContactListActivity impleme
        switch (v.getId()){
             case R.id.rl_addpeople:
                 Intent intent = new Intent(this, FriendsContactsActivity.class);
-               /* if (fistAdd){
-                    intent.putExtra("deptid", AddressList.deptId);//第一次取固定的
-                    fistAdd = false;
-                    Log.d("zxy :", "91 : ManagerColleagueActivity : onClick : deptid = "+idDep);
-                }else if(!android.text.TextUtils.isEmpty(idDep)) {
-                    intent.putExtra("deptid",idDep);
-                }else{
-                    intent.putExtra("deptid", AddressList.deptId);
-                }*/
                 String id=AddressList.deptId;
                 if (list!=null&&list.size()>0){
                     BaseSearch search = list.get(0);
                     String s[] = new String[]{};
                     if (search instanceof CompanyContactListEntity) {
                         CompanyContactListEntity entity = (CompanyContactListEntity) search;
-                        s = entity.treePath.split("/");
-                        if (s.length>=2){
-                            id = s[s.length-1];
+                        if (!TextUtils.isEmpty(entity.treePath)&&entity.treePath.contains("/")){
+                            s = entity.treePath.split("/");
+                            if (s.length>=2){
+                                id = s[s.length-1];
+                            }
                         }
-
                     } else if (search instanceof CompanyDepment.Data.Depaments) {
                         CompanyDepment.Data.Depaments entity = (CompanyDepment.Data.Depaments) search;
-                        s =entity.treePath.split("/");
-                        if (s.length>=2) {
-                            id = s[s.length - 2];
+                        if (!TextUtils.isEmpty(entity.treePath)&&entity.treePath.contains("/")){
+                            s =entity.treePath.split("/");
+                            if (s.length>=2) {
+                                id = s[s.length - 2];
+                            }
                         }
                     }
                 }else {
